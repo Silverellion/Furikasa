d97eb3c1a9ed8fce269586009e182e5b54c4b9a9
Hoang Tri Minh Tam <hoangtriminhtam@gmail.com>
2025-11-20 18:13:36 +0900

    Project Setup

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Furikasa
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
